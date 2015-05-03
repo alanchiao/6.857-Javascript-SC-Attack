8a06d61b8e527e7221f8bdc85564373de364e252
Jeffrey Sun <jeffreysun@Jeffreys-MacBook-Pro.local>
2015-05-03 12:25:34 -0400

    set operations for cacheProfiler

--- cacheProfiler.js
+++ cacheProfiler.js
@@ -1,3 +1,4 @@
+
 // Algorithm one from the paper
 // Computer Specs : Alan - 2.3GHz Intel Core i7 - 6 MB L3 cache
 //                  Might have 8192 cache sets (2^13) with 12 lines of 64 bytes (2^6)
@@ -10,16 +11,32 @@ var primeBuffer = new ArrayBuffer(8192 * 1024);
 var primeView = new DataView(primeBuffer); 
 var x = 1; // page in question mb
 
+// S[i] is true if "i" is in "x"'s set. S[i] is false if untried.
+var S = {}
+for (var i=0; i<8192*1023/offset; i++) {
+  S[i] = false;
+}
+
 // L3 cache line size
 var offset = 64;
 
-while (true) {
-  // iteratively access all members of S
-  var current;
-  for (var i = 0; i < ((8192 * 1023) / offset); i++) {
-    current = probeView.getUint32(i * offset);
-  }
+function accessMembers(set) {
+  set.forEach(function(member) {
+    probeView.getUint32(member);
+  });
+}
 
+function removeRandom(set) {
+  untried = Object.keys(set).filter(function(member) {return !set[member];});
+  s = Math.floor(Math.random() * (Object.keys(untried).length + 1))
+  delete set[s]
+}
+
+while Object.keys(S).length > 12 {
+  // iteratively access all members of S
+  accessMembers(S);
+
+  var current;
   // first retrieval, hypothesized to be from RAM
   var startTime1 = window.performance.now();
   current = primeView.getUint32(x);
@@ -27,9 +44,9 @@ while (true) {
   var diffTime1 = endTime1 - startTime1;
 
   // select random page s from S and remove it
-
+  removeRandom(S)
   // iteratively access all members of S\s
-
+  accessMembers(S);
   // second retrieval, hope it is significantly faster (if not, then not in same cache set as x)
   var startTime2 = window.performance.now();
   current = primeView.getUint32(x);
@@ -37,6 +54,6 @@ while (true) {
   var diffTime2 = endTime2 - startTime2;
 
   if (diffTime1 - diffTime2 > threshold) { // then place s back into S, in same cache set
-    
+    S[s] = true;
   }
 }
